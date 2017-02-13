76a205d82aac1aa45c0d7dd0275c5070ebe5f6b3
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-02-13 16:11:35 +0100

    initial gitignore

--- pixiu/cli.py
+++ pixiu/cli.py
@@ -1,10 +1,8 @@
 import click
 
-
 @click.command()
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
 @click.argument('name', default='world', required=False)
 def main(name, as_cowboy):
-    """A company CLI for dependency managment and deployment."""
     greet = 'Howdy' if as_cowboy else 'Hello'
     click.echo('{0}, {1}.'.format(greet, name))
