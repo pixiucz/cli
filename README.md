651eb39f2218ba02d8ed9f11b1ef8efd8317d5f1
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-02-13 16:01:03 +0100

    Initial cmdline

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pixiu CLI
+
+A company CLI for dependency managment and deployment.
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ pixiu --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+A company CLI for dependency managment and deployment.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='pixiu-cli',
+    version='0.1.0',
+    url='https://github.com/pixiucz/cli',
+    license='MIT',
+    author='Tomas Ondrus',
+    author_email='ondrus@pixiu.cz',
+    description='A company CLI for dependency managment and deployment.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'pixiu = pixiu.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
